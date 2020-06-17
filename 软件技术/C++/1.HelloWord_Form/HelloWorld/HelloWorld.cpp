6a50f12a111fbe76078dcde889886d6d100019cd
Tiger <809936987@qq.com>
2020-06-17 19:50:41 +0800

    C++
    提交拷贝析构函数操作

--- 软件技术/C++/1.HelloWord_Form/HelloWorld/HelloWorld.cpp
+++ 软件技术/C++/1.HelloWord_Form/HelloWorld/HelloWorld.cpp
@@ -2,59 +2,59 @@
 //
 
 #include <iostream>
-#include <limits>
 
 using namespace std;
+class Line
+{
+public:
+    int getLength(void);
+    Line(int len);             // 简单的构造函数
+    Line(const Line& obj);      // 拷贝构造函数
+    ~Line();                     // 析构函数
+
+private:
+    int* ptr;
+};
+
+// 成员函数定义，包括构造函数
+Line::Line(int len)
+{
+    cout << "调用构造函数" << endl;
+    // 为指针分配内存
+    ptr = new int;
+    *ptr = len;
+}
+
+Line::Line(const Line& obj)
+{
+    cout << "调用拷贝构造函数并为指针 ptr 分配内存" << endl;
+    ptr = new int;
+    *ptr = *obj.ptr; // 拷贝值
+}
+
+Line::~Line(void)
+{
+    cout << "释放内存" << endl;
+    delete ptr;
+}
+int Line::getLength(void)
+{
+    return *ptr;
+}
+
+void display(Line obj)
+{
+    cout << "line 大小 : " << obj.getLength() << endl;
+}
 
 int main()
 {
+    Line line(10);
+
+    display(line);
+
     cout << "Hello World!\n";
-    cout << "type: \t\t" << "************size**************" << endl;
-    cout << "bool: \t\t" << "所占字节数：" << sizeof(bool);
-    cout << "\t最大值：" << (numeric_limits<bool>::max)();
-    cout << "\t\t最小值：" << (numeric_limits<bool>::min)() << endl;
-    cout << "char: \t\t" << "所占字节数：" << sizeof(char);
-    cout << "\t最大值：" << (numeric_limits<char>::max)();
-    cout << "\t\t最小值：" << (numeric_limits<char>::min)() << endl;
-    cout << "signed char: \t" << "所占字节数：" << sizeof(signed char);
-    cout << "\t最大值：" << (numeric_limits<signed char>::max)();
-    cout << "\t\t最小值：" << (numeric_limits<signed char>::min)() << endl;
-    cout << "unsigned char: \t" << "所占字节数：" << sizeof(unsigned char);
-    cout << "\t最大值：" << (numeric_limits<unsigned char>::max)();
-    cout << "\t\t最小值：" << (numeric_limits<unsigned char>::min)() << endl;
-    cout << "wchar_t: \t" << "所占字节数：" << sizeof(wchar_t);
-    cout << "\t最大值：" << (numeric_limits<wchar_t>::max)();
-    cout << "\t\t最小值：" << (numeric_limits<wchar_t>::min)() << endl;
-    cout << "short: \t\t" << "所占字节数：" << sizeof(short);
-    cout << "\t最大值：" << (numeric_limits<short>::max)();
-    cout << "\t\t最小值：" << (numeric_limits<short>::min)() << endl;
-    cout << "int: \t\t" << "所占字节数：" << sizeof(int);
-    cout << "\t最大值：" << (numeric_limits<int>::max)();
-    cout << "\t最小值：" << (numeric_limits<int>::min)() << endl;
-    cout << "unsigned: \t" << "所占字节数：" << sizeof(unsigned);
-    cout << "\t最大值：" << (numeric_limits<unsigned>::max)();
-    cout << "\t最小值：" << (numeric_limits<unsigned>::min)() << endl;
-    cout << "long: \t\t" << "所占字节数：" << sizeof(long);
-    cout << "\t最大值：" << (numeric_limits<long>::max)();
-    cout << "\t最小值：" << (numeric_limits<long>::min)() << endl;
-    cout << "unsigned long: \t" << "所占字节数：" << sizeof(unsigned long);
-    cout << "\t最大值：" << (numeric_limits<unsigned long>::max)();
-    cout << "\t最小值：" << (numeric_limits<unsigned long>::min)() << endl;
-    cout << "double: \t" << "所占字节数：" << sizeof(double);
-    cout << "\t最大值：" << (numeric_limits<double>::max)();
-    cout << "\t最小值：" << (numeric_limits<double>::min)() << endl;
-    cout << "long double: \t" << "所占字节数：" << sizeof(long double);
-    cout << "\t最大值：" << (numeric_limits<long double>::max)();
-    cout << "\t最小值：" << (numeric_limits<long double>::min)() << endl;
-    cout << "float: \t\t" << "所占字节数：" << sizeof(float);
-    cout << "\t最大值：" << (numeric_limits<float>::max)();
-    cout << "\t最小值：" << (numeric_limits<float>::min)() << endl;
-    cout << "size_t: \t" << "所占字节数：" << sizeof(size_t);
-    cout << "\t最大值：" << (numeric_limits<size_t>::max)();
-    cout << "\t最小值：" << (numeric_limits<size_t>::min)() << endl;
-    cout << "string: \t" << "所占字节数：" << sizeof(string) << endl;
-    // << "\t最大值：" << (numeric_limits<string>::max)() << "\t最小值：" << (numeric_limits<string>::min)() << endl;  
-    cout << "type: \t\t" << "************size**************" << endl;
+
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
